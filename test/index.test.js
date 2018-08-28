ef6852db12bbd77c687954b316b0618aded35716
Zubin Cheung <zubin@zubin-pc.local>
2018-08-28 12:01:50 +0800

    v1.2.0

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,20 @@
 const Redis = require('../index');
 
+const redis = Redis.init({
+  host: '127.0.0.1',
+  port: 6379,
+  password: '',
+  db: 0,
+  connectTimeout: 1000,
+  lazyConnect: false,
+  keyPrefix: '',
+});
+
 describe('redis test', () => {
+  afterAll(() => {
+    return redis.end();
+  });
   it('set:', async () => {
-    const redis = Redis.init({
-      host: '127.0.0.1',
-      port: 6379,
-      password: '',
-      db: 0,
-      connectTimeout: 1000,
-      lazyConnect: false,
-      keyPrefix: '',
-    });
     await redis.setData('test', 'hello', 60);
     await redis.setData('test1', 'hello', 60);
     await redis.setData('test2', 'hello', 60);
@@ -20,15 +24,6 @@ describe('redis test', () => {
   });
 
   it('getJson:', async () => {
-    const redis = Redis.init({
-      host: '127.0.0.1',
-      port: 6379,
-      password: '',
-      db: 0,
-      connectTimeout: 1000,
-      lazyConnect: false,
-      keyPrefix: '',
-    });
     await redis.setData('b', JSON.stringify({ a: 1 }), 60);
 
     expect(redis.getJsonData('b')).resolves.toEqual({ a: 1 });
@@ -38,37 +33,17 @@ describe('redis test', () => {
   });
 
   it('delete:', async () => {
-    const redis = Redis.init({
-      host: '127.0.0.1',
-      port: 6379,
-      password: '',
-      db: 0,
-      connectTimeout: 1000,
-      lazyConnect: false,
-      keyPrefix: '',
-    });
-
     const result = await redis.deleteKey('test');
     expect(result.rows).toBe(1);
   });
 
   it('deleteMany:', async () => {
-    const redis = Redis.init({
-      host: '127.0.0.1',
-      port: 6379,
-      password: '',
-      db: 0,
-      connectTimeout: 1000,
-      lazyConnect: false,
-      keyPrefix: '',
-    });
-
     const result = await redis.deleteKey('test*');
     expect(result.rows).toBe(3);
   });
 
   it('deleteMany2:', async () => {
-    const redis = new Redis({
+    const r = new Redis({
       host: '127.0.0.1',
       port: 6379,
       password: '',
@@ -80,25 +55,18 @@ describe('redis test', () => {
 
     const tasks = [];
     for (let i = 0; i < 1000; i++) {
-      tasks.push(redis.setData(`test:${i}`, 'hello', 60));
+      tasks.push(r.setData(`test:${i}`, 'hello', 60));
     }
 
     await Promise.all(tasks);
 
-    const result = await redis.deleteKey('*test*');
+    const result = await r.deleteKey('*test*');
     expect(result.rows).toBe(1000);
+
+    await r.end();
   });
 
   it('should deleteKey not found key', async () => {
-    const redis = Redis.init({
-      host: '127.0.0.1',
-      port: 6379,
-      password: '',
-      db: 0,
-      connectTimeout: 1000,
-      lazyConnect: false,
-      keyPrefix: '',
-    });
     const r = await redis.deleteKey('*head*');
     expect(r).toEqual({ keys: [], rows: 0 });
   });
